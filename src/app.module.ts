f3fc4b2e25c35aac563696edbb7ab2cb4c00c026
Joominseok <jumseok12@gmail.com>
2023-02-21 10:59:12 +0900

    ignore

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BoardModule } from './board/board.module';
 import { TypeormConfigService } from './config/typeorm.config.service';
+import { UserModule } from './user/user.module';
 
 // @가 붙는 키워드를 데코레이터라고 함, 데코레이터는 해당 클래스나 함수가 어떤 역할을 수행하는 지 nestjs에게 알려주는 것
 @Module({ 
@@ -13,7 +14,8 @@ import { TypeormConfigService } from './config/typeorm.config.service';
     ConfigModule.forRoot({isGlobal : true}),
     // 제일 먼저 보이는 모듈에 데이터베이스 접속 정보가 노출되기 때문에 nestjs/config로 감춤
     TypeOrmModule.forRootAsync({useClass : TypeormConfigService}),
-    BoardModule
+    BoardModule,
+    UserModule
   ], 
   // controllers는 이 모듈은 어떤 컨트롤러를 쓰겠다.
   controllers: [AppController], 
